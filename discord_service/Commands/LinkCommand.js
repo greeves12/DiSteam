f4989acfb12c3bdc4f767eddbdb8de7de732a46d
greeves12 <tategreeves@gmail.com>
2023-10-09 20:27:50 -0600

    Created a authentication token model. Authenticated Atlas with the application.

--- discord_service/Commands/LinkCommand.js
+++ discord_service/Commands/LinkCommand.js
@@ -1,11 +1,32 @@
 const {SlashCommandBuilder} = require('@discordjs/builders');
+const {fetch} = require('node-fetch');
+require('dotenv').config();
 
+/*
+* Purpose is to create a command that links a user's discord and steam account.
+*
+* We make a post request with the API key to retrieve a state token for completing the link on the webserver.
+* Assuming that the user is already linked, an empty JSON is sent back from the server, if account is NOT linked then it will send back a token
+* to form the URL that is then sent to the user who executed the command.
+*/
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("link")
-    .setDescription("Link you Steam with Discord"),
+    .setDescription("Link your Steam with Discord"),
     async execute (interaction){
-        interaction.reply("pong");
+        fetch('', { 
+            method: 'POST', 
+            body: JSON.stringify({ 
+                api_key: process.env.API_KEY, 
+                id: interaction.user.id
+        }), 
+        headers: { 
+            'Content-type': 'application/json; charset=UTF-8', 
+        }, 
+    }) 
+    .then((response) => response.json()) 
+    .then((json) => interaction.reply({content: `Click the following URL to complete account link.\n https://link.com/openid?state=${json.token}`, ephemeral:  true})) 
+    .catch(err => console.log(err))
+    
     }
-
 };
--- webservice/src/main/java/com/tategreeves/webservice/WebserviceApplication.java
+++ webservice/src/main/java/com/tategreeves/webservice/WebserviceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceApplication.class, args);
 	}
