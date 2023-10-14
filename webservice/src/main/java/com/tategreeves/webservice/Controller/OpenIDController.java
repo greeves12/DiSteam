9077a0ec65033141f55bfa309331c2ed6fece16f
greeves12 <tategreeves@gmail.com>
2023-10-14 13:53:17 -0600

    - Prevent new tab from opening.

--- webservice/src/main/java/com/tategreeves/webservice/Controller/OpenIDController.java
+++ webservice/src/main/java/com/tategreeves/webservice/Controller/OpenIDController.java
@@ -40,7 +40,7 @@ public class OpenIDController {
 
             OpenIdManager manager = new OpenIdManager();
 
-            manager.setRealm("DiSteam");
+            manager.setRealm("https://steam-auth-bot-production.up.railway.app/openid");
             manager.setReturnTo("https://steam-auth-bot-production.up.railway.app/openid?login=verify");
 
             Endpoint endpoint = manager.lookupEndpoint("https://steamcommunity.com/openid");
