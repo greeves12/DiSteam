fa4ae63fb58d9e8d1425c6fae2a95460a396d2f4
greeves12 <tategreeves@gmail.com>
2023-11-05 22:10:51 -0700

    - Added web requests for authenticated users.

--- webservice/src/main/java/com/tategreeves/webservice/Controller/WebController.java
+++ webservice/src/main/java/com/tategreeves/webservice/Controller/WebController.java
@@ -1,12 +1,12 @@
 package com.tategreeves.webservice.Controller;
 
 import com.tategreeves.webservice.Model.Panel;
+import com.tategreeves.webservice.Model.ServerConfig;
 import com.tategreeves.webservice.Service.PanelService;
 import com.tategreeves.webservice.Service.ServerConfigService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class WebController {
@@ -17,4 +17,29 @@ public class WebController {
     private ServerConfigService serverConfigService;
 
 
+    @GetMapping("/service/requests/get/config")
+    public ServerConfig getConfig(@RequestHeader("discord-id") String discord_id, @RequestHeader("auth-token") String authToken, HttpServletResponse response, @RequestHeader("server-id") String serverId){
+        Panel client = new Panel(discord_id);
+        client.setAuth_token(authToken);
+
+        if(!authentication.tokenExists(client)){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return null;
+        }
+
+        return serverConfigService.getConfig(serverId, client.getDiscord_id());
+    }
+
+    @PostMapping("/service/requests/post/config")
+    public void updateConfig(@RequestHeader("discord-id") String discord_id, @RequestHeader("auth-token") String authToken, HttpServletResponse response, @RequestBody ServerConfig config){
+        Panel client = new Panel(discord_id);
+        client.setAuth_token(authToken);
+
+        if(!authentication.tokenExists(client)){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        serverConfigService.updateConfig(config);
+    }
 }
